bdf35d72c979d7cb10ef864275983430b3975da4
Moritz Kork <moritz.kork@gmail.com>
2020-05-21 09:26:22 +0200

    rails day 3 - ex 2 V2

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+  restcat = ["chinese", "italian", "japanese", "french", "belgian"]
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: { in: restcat }
 end
--- db/migrate/20200520105108_create_reviews.rb
+++ db/migrate/20200520105108_create_reviews.rb
@@ -4,7 +4,6 @@ class CreateReviews < ActiveRecord::Migration[6.0]
       t.text :context
       t.integer :rating
       t.references :restaurant, null: false, foreign_key: true
-
       t.timestamps
     end
   end
